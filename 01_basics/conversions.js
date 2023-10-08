962ead07a9a24956e09e797c86845118cea06a5b
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-08 20:44:31 +0000

    container config done

---
+++ 01_basics/conversions.js
@@ -0,0 +1,16 @@
+let score = "34";
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(score));
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+let name = "";
+let convertBoolean = Boolean(name);
+console.log(convertBoolean);
