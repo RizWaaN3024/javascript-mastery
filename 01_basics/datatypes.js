71058d189f9a59bc413e1b42137fd9dec29b3c7e
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-08 20:33:44 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"Use Strict";//treat all JS code as newer version
+
+let name = "Rizwaan";
+let age = 21;
+let isLoggedIn = false;
+
+/*
+Primitive Datatypes
+number 
+bigint
+string
+boolean
+null
+undefined
+symbol --used for uniqueness 
+*/
+
+console.log(typeof(name));
+console.log(typeof(undefined));//undefined
+console.log(typeof(null));//object
