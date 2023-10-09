4497e7cf676e1a7cc4aa13e14d2ba8c161eed65e
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-09 20:20:21 +0000

    container config done

---
+++ 01_basics/operations.js
@@ -0,0 +1,24 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//this line means 2 to the power 2
+console.log(2%3);
+
+
+let str = "hello";
+let str2 = " hi";
+let str3 = str + str2;
+console.log(str3);
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+//comparisons
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+//Strict Check === (it checks datatype also)
+console.log("2" === 2);
