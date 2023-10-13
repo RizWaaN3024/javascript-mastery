362cea12c992c477c0dc161c58d54a61a8bc91f3
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-13 19:15:39 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -25,3 +25,23 @@ function user(userName){
 }
 
 console.log(loginMessage("Rizwaan"));
+
+//when u dont know how many parameters user is going to enter (Rest Operator)
+function calculateCartPrice(...num1){
+    return num1;
+
+}
+console.log(calculateCartPrice(200,400,500,600));
+//(...)--rest operator returns an array
+
+//pass objects to functions
+
+const userTwo = {
+    name:"Rizwaan",
+    age:21
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and age is ${anyobject.age}`);
+}
+
+handleObject(userTwo);
