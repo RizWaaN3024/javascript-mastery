eb95e1dfc86de44bb53ddec3753ba41a10206bb5
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-14 20:39:28 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,12 @@
+//Immediately Invoked Function Expressions
+
+//syntax
+(function chai(){
+    console.log('DB CONNECTED')
+})();//use semicolon to end the iife expression
+
+//arrow function
+(() =>{
+    console.log("DB2 CONNECTED");
+})();
+
