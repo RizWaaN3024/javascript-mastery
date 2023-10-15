2dc804fe466c8e6f645aba0d22bd44a59faec085
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-10-15 19:42:24 +0000

    one

---
+++ 04_control-flow/one.js
@@ -0,0 +1,15 @@
+//if statement
+
+// if(condition){//if condition is true then enter this block and execute
+
+// }
+
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+    console.log("Logged In");
+}
+
+//=-assignment
+//==-is equal(ignores the type)
+//!=-not equal
+//===-checks the type also
