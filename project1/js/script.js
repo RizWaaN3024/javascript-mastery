232b68091e5be69cd2aa5a76648e3852393fd151
RizWaaN3024 <108953802+RizWaaN3024@users.noreply.github.com>
2023-11-22 02:27:46 +0530

    Add files via upload

---
+++ project1/js/script.js
@@ -0,0 +1,21 @@
+const box = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+box.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'gray'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'aqua'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
